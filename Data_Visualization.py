ea3d775f84d034754b68e11db19f7992101d87cf
skywalker07 <cp2708@columbia.edu>
2016-12-05 05:37:56 -0500

    Other python test files

--- Data_Visualization.py
+++ Data_Visualization.py
@@ -5,16 +5,17 @@ import glob
 import matplotlib.pyplot as plt
 
 path = 'C:/Users/Chaitu/PycharmProjects/PythonLessons/PythonScrapFiles/datavis/12'
-
-all_files = glob.glob(os.path.join(path, 'AXWW21-MH.*.ret')) # advisable to use os.path.join as this makes concatenation OS independent
+styles = ['Value', 'Leverage', 'Growth', 'Size', 'Volatility', 'Liquidity', 'Exchange Rate Sensitivity']
+all_files_return = glob.glob(os.path.join(path, 'AXWW21-MH.*.ret')) # advisable to use os.path.join as this makes concatenation OS independent
+all_files_exp = glob.glob(os.path.join(path, 'AXWW21-MH.*.exp'))
 
 #Dates - list comprehension
-dates = [pd.to_datetime(filepath[-12:-4], format = '%Y%m%d') for filepath in all_files]
+dates = [pd.to_datetime(filepath[-12:-4], format = '%Y%m%d') for filepath in all_files_return]
 #   count = 0
 
 return_df = pd.DataFrame()
 
-for file_ in all_files:
+for file_ in all_files_return:
    # df = pd.read_table('file:///C:/Users/cpattapagala/PycharmProjects/PythonLessons/PythonScrapFiles/datavis/12/AXWW21-MH.20151201.ret', skiprows=range(3), sep='|', names = ['FactorName', 'Return', 'Cumulative Return'], header= None, verbose=True)
 #    count = count + 1
 
@@ -22,20 +23,28 @@ for file_ in all_files:
     #Can be scalable
     return_df = return_df.append(df['Return'].transpose())
 
-
 #indexing and column names
 return_df.columns = df['FactorName'].transpose()
 return_df.index = dates
 return_df.columns.name = ''
 return_df.index.name = 'Date'
 
+for exp_file in all_files_exp:
+    df = pd.read_table(exp_file, skiprows=range(2), sep = '|', verbose=False)
+
+
 #  Data Visualization
 '''
 melt works by taking observations that are spread across columns (away_team, home_team), and melting them down into one column with multiple rows. However, we don't want to lose the metadata (like game_id and date) that is shared between the observations. By including those columns as id_vars, the values will be repeated as many times as needed to stay with their observations.
 '''
 
-# df = pd.melt(return_df.reset_index(), id_vars=['Date'], var_name='Factor Name')
-
 #Plotting
+plt.close('all')
+fig, ax = plt.subplots(1)
+plt.grid(True)
+ax.plot(return_df[styles])
+fig.autofmt_xdate()
 
-return_df.plot
+import matplotlib.dates as mdates
+ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
+
